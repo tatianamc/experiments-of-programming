121fdd1fee245e3c2f12b8aeacd9f03db8a0a699
Igor Nesterenya <nesterione@gmail.com>
2015-06-21 01:46:28 +0300

    Some changes

--- Courcera/CleaningData/run_analysis.R
+++ Courcera/CleaningData/run_analysis.R
@@ -1,4 +1,7 @@
 run <- function() {
+  #
+  # Step 1: Merges the training and the test sets to create one data set.
+  #
   testData <- read.table("./data/test/X_test.txt")
   trainData <- read.table("./data/train/X_train.txt")
   dataSet <- rbind(testData, trainData)
@@ -11,29 +14,41 @@ run <- function() {
   yTrain <- read.table("./data/train/y_train.txt")
   ySet <- rbind(yTest, yTrain)
   
-  #dataSet <- read.table("./data/test/X_test.txt")
-  #subjectSet <- read.table("./data/test/subject_test.txt")
-  #ySet <- read.table("./data/test/y_test.txt")
-  
-  
   colnames(subjectSet) <- "subject"
   colnames(ySet) <- "activity"
   
-  
-  
-  # Step 2
-  
+  #
+  # Step 2: Extracts only the measurements on the mean and standard deviation for each measurement. 
+  #
   features <- read.table("./data/features.txt")
-  
   dataSet <- dataSet[,grepl("(mean\\(\\)|std\\(\\))",features[,2])]
   
+  
+  #
+  # Step 4: Appropriately labels the data set with descriptive variable names. 
+  #
   variableNames <- features[grepl("(mean\\(\\)|std\\(\\))",features[,2]),2]
   colnames(dataSet) <- variableNames
   
+  # Bind y and activity
   dataSet <- cbind(dataSet, subjectSet)
   dataSet <- cbind(dataSet, ySet)
   
+  
+  #
+  # Step 3: Uses descriptive activity names to name the activities in the data set
+  #
+  activityLabels <- read.table("./data/activity_labels.txt")
+  dataSet$activity <- factor(dataSet$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+  
+  
+  #
+  # Step 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+  #
+  # get formula for aggregate 
   sform <- as.formula(paste("cbind(", paste("`", names(dataSet)[1:66], "`", sep="", collapse=","),")~subject+activity")) 
   cleardata <- aggregate(sform, data=dataSet, mean)
-  cd<<-cleardata
+  
+  # return result
+  cleardata
 }

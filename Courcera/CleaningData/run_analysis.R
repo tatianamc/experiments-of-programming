8d7cea75c32855079a1841df728fbfe73b908d79
Igor Nesterenya <nesterione@gmail.com>
2015-06-23 21:28:25 +0300

    Add mongo python work

--- Courcera/CleaningData/run_analysis.R
+++ Courcera/CleaningData/run_analysis.R
@@ -46,7 +46,7 @@ run <- function() {
   # Step 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
   #
   # get formula for aggregate 
-  sform <- as.formula(paste("cbind(", paste("`", names(dataSet)[1:66], "`", sep="", collapse=","),")~subject+activity")) 
+  sform <<- as.formula(paste("cbind(", paste("`", names(dataSet)[1:66], "`", sep="", collapse=","),")~subject+activity")) 
   cleardata <- aggregate(sform, data=dataSet, mean)
   
   # return result

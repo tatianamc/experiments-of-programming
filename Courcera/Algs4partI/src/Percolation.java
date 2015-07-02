c9e0409b2bcfb4387b118a01ad86dc4f7cb8b257
Igor Nesterenya <nesterione@gmail.com>
2015-07-03 01:54:35 +0300

    Algs4partI solved task 1

--- Courcera/Algs4partI/src/Percolation.java
+++ Courcera/Algs4partI/src/Percolation.java
@@ -1,48 +1,195 @@
 
 public class Percolation {
     private int N;
+    private WeightedQuickUnionUF uf;
+    private int top;
+    private int bottom;
+
+    private int[][] matrix;
+    private int[] bottomRow;
+    private int pointer;
+    private boolean[] flags;
+    private final static int CELL_OPEN = 1;
+    private final static int CELL_CLOSED = 0;
+
 
     // create N-by-N grid, with all sites blocked
     public Percolation(int N) {
-        if(N<=0) throw new IllegalArgumentException();
+        if (N <= 0) throw new IllegalArgumentException();
         this.N = N;
+
+        int countNodes = N * N + 2;
+
+        this.uf = new WeightedQuickUnionUF(countNodes);
+        this.top = countNodes - 2;
+        this.bottom = countNodes - 1;
+
+        this.matrix = new int[N][N];
+        bottomRow = new int[N];
+        flags = new boolean[N * N];
+        for (int i = flags.length - N; i < flags.length; i++) {
+            flags[i] = true;
+        }
     }
 
-    // open site (row i, column j) if it is not open already
+    // DONE open site (row i, column j) if it is not open already
     public void open(int i, int j) {
+        //System.out.println("COUTN: "+uf.count());
         checkIndex(i);
         checkIndex(j);
 
+        if (!isOpen(i, j)) {
+
+            matrix[i - 1][j - 1] = CELL_OPEN;
+
+            int flatIdxCenter = toFlatIndex(i, j);
+
+            if (i == 1) {
+                uf.union(flatIdxCenter, this.top);
+            }
+
+            // TODO
+            /*if (i == N) {
+                uf.union(flatIdxCenter, this.bottom);
+            }*/
+            /*if (i == N) {
+                bottomRow[pointer++] = j;
+            }*/
+
+            int[][] idx = {
+                    {i - 1, j},
+                    {i + 1, j},
+                    {i, j + 1},
+                    {i, j - 1}
+            };
+
+
+            boolean conToBot = false;
+            for (int[] anIdx : idx) {
+
+                int curI = anIdx[0];
+                int curJ = anIdx[1];
+
+                if (exists(curI, curJ) && isOpen(curI, curJ)) {
+                    int flatIdx = toFlatIndex(curI, curJ);
+
+                    int idd = uf.find(flatIdx);
+
+                    conToBot = conToBot || flags[idd];
+                   // System.out.println(flags[idd]);
+                    //flags[idd] = false;
+                    //System.out.println(flags[idd]);
+                }
+            }
+
+
+            for (int[] anIdx : idx) {
+                int curI = anIdx[0];
+                int curJ = anIdx[1];
+
+
+                if (exists(curI, curJ) && isOpen(curI, curJ)) {
+                    int flatIdx = toFlatIndex(curI, curJ);
+                    uf.union(flatIdx, flatIdxCenter);
+                }
+            }
+
+            //System.out.println("**** *** *"+ flags[flatIdxCenter]+"===="+flatIdxCenter);
+            int newRoot = uf.find(flatIdxCenter);
+            flags[newRoot] = conToBot || flags[flatIdxCenter];
+
+
+            /*for (int[] anIdx : idx) {
+
+                int curI = anIdx[0];
+                int curJ = anIdx[1];
+
+                if (exists(curI, curJ) && isOpen(curI, curJ)) {
+                    int flatIdx = toFlatIndex(curI, curJ);
+
+                    int idd = uf.find(flatIdx);
+
+                    flags[idd]= conToBot;
+
+                    //flags[idd] = false;
+                    //System.out.println(flags[idd]);
+                }
+            }*/
+
+
+            if(!isPercolates&& uf.connected(flatIdxCenter,this.top)&&flags[newRoot] ) {
+                isPercolates = true;
+            }
+           /* for (int col = 0; col < pointer&&!isPercolates; col++) {
+                //int idx = matrix[N - 1][bottomRow[i]-1];
+                int fidx = toFlatIndex(N, bottomRow[col] );
+                if (uf.connected(fidx, this.top)) {
+                    uf.union(this.bottom, fidx);
+                    isPercolates = true;
+
+                }
+            }*/
+
+        }
+    }
+
+    private boolean exists(int i, int j) {
+        return (0 < i && i <= N) && (0 < j && j <= N);
     }
 
     private void checkIndex(int i) {
-        if(i<1||i>N)
-            throw new IndexOutOfBoundsException();
+        if (i <= 0 || i > N) throw new IndexOutOfBoundsException("row index i out of bounds");
     }
 
-    // is site (row i, column j) open?
+    // DONE is site (row i, column j) open?
     public boolean isOpen(int i, int j) {
         checkIndex(i);
         checkIndex(j);
 
-        return false;
+        return (matrix[i - 1][j - 1] != CELL_CLOSED);
     }
 
-    // is site (row i, column j) full?
+    private int toFlatIndex(int i, int j) {
+        return (i - 1) * N + (j - 1);
+    }
+
+    // DONE is site (row i, column j) full?
     public boolean isFull(int i, int j) {
         checkIndex(i);
         checkIndex(j);
-
-        return false;
+        int flatIndex = toFlatIndex(i, j);
+        //System.out.println(i+" "+ j+":"+ top+" "+ flatIndex);
+        return uf.connected(top, flatIndex);
     }
 
-    // does the system percolate?
+    // DONE does the system percolate?
     public boolean percolates() {
-        return false;
+
+        // Можно хранить список открытых элементов и безать только по ним
+        /*for(int bitem =0; bitem < N;bitem++) {
+            if(matrix[N-1][bitem]==CELL_OPEN_BOTTOM) {
+
+                int fidx = toFlatIndex(N,bitem+1);
+                if(uf.connected(fidx,this.top)) {
+                    uf.union(this.bottom,fidx);
+                }
+            }
+        }*/
+
+        /*if (!isPercolates) {
+            int idx = uf.find(top);
+            System.out.println("^^^^" + idx);
+            isPercolates = (N * N - N - 2) < idx && idx < (N * N - 2);
+        }*/
+
+        //System.out.format(">>> >>> %d %d %d\n", uf.find(top), uf.find(bottom), top);
+        return isPercolates;   //uf.connected(top, bottom);   //isPercolates;  //
     }
+
+    private boolean isPercolates = false;
 
     // test client (optional)
-    public static void main(String[] args ) {
+    public static void main(String[] args) {
 
     }
 }
---
+++ Courcera/Algs4partI/src/PercolationStats.java
@@ -0,0 +1,117 @@
+/**
+ * Created by igor on 01.07.2015.
+ */
+public class PercolationStats {
+
+    private double mean = Double.NaN;
+    private double stddev = Double.NaN;
+    private int T;
+    private double[] xt;
+
+    /**
+     * TODO perform T independent experiments on an N-by-N grid
+     * @param N
+     * @param T
+     */
+    public PercolationStats(int N, int T) {
+        validate(N);
+        validate(T);
+        this.T = T;
+
+
+        xt = new double[T];
+
+        int n = N*N;
+
+        for(int i = 0; i< T; i++) {
+            int x=0;
+            Percolation percolation = new Percolation(N);
+            while (!percolation.percolates()) {
+                x++;
+
+                int row = StdRandom.uniform(1,N+1);
+                int col = StdRandom.uniform(1,N+1);
+                while (percolation.isOpen(row,col)) {
+                    row = StdRandom.uniform(1,N+1);
+                    col = StdRandom.uniform(1,N+1);
+                }
+                percolation.open(row,col);
+            }
+
+
+            xt[i] = ((double)x)/(double)(N*N);
+        }
+    }
+
+    private void validate(int N) {
+        if(N <=0) throw new java.lang.IllegalArgumentException("args can't be <= 0");
+    }
+
+    /**
+     * TODO sample mean of percolation threshold
+     * @return
+     */
+    public double mean() {
+
+        if(Double.isNaN(mean)) {
+            double sum = 0;
+            for(int i = 0;i< T;i++) {
+                sum += xt[i];
+            }
+
+
+            mean = sum/T;
+        }
+
+
+        return mean;
+    }
+
+    /**
+     * sample standard deviation of percolation threshold
+     * @return
+     */
+    public double stddev() {
+        if(Double.isNaN(stddev)) {
+            double sum = 0;
+            for(int i = 0;i< T;i++) {
+                sum += (xt[i]-mean())*(xt[i]-mean());
+            }
+            stddev = Math.sqrt( sum/(T-1));
+        }
+
+        return stddev;
+    }
+
+    /**
+     * TODO low  endpoint of 95% confidence interval
+     * @return
+     */
+    public double confidenceLo(){
+        return mean()- 1.96*stddev()/ Math.sqrt(T) ;
+    }
+
+    /**
+     * TODO high endpoint of 95% confidence interval
+     * @return
+     */
+    public double confidenceHi() {
+        return mean()+ 1.96*stddev()/ Math.sqrt(T);
+    }
+
+    /**
+     * test client (described below)
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        if(args.length<2) throw new IllegalArgumentException("wait two numbers");
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+
+        PercolationStats stats = new PercolationStats(N,T);
+        System.out.println("mean: "+stats.mean());
+        System.out.println("stddve: "+stats.stddev());
+        System.out.println("95% confidence interval: "+stats.confidenceLo()+" "+stats.confidenceHi());
+    }
+}

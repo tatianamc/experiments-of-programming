4c408236243833ecee398f9f32c77f2ce89106db
Igor Nesterenya <nesterione@gmail.com>
2015-12-09 03:13:47 +0300

    Finish Angular demo

--- C#/Rest/RestViaMVC/RestViaMVC/Scripts/App.js
+++ C#/Rest/RestViaMVC/RestViaMVC/Scripts/App.js
@@ -9,9 +9,30 @@ var controllersM = angular.module('controllersM', []);
 
 controllersM.controller('MainCntl', function ($scope, $http) {
 
-    //$scope.ticket = { "Id": 32, "Name": "wer", "Number": "qeeq" };
+    $scope.enableUpdateMode = function (ticket) {
+        $scope.ticket = ticket;
+        $scope.isEditing = true;
+        $scope.isAdding = false;
+        $scope.isViewMode = false;
+    }
+
+    $scope.enableViewMode = function () {
+        $scope.ticket = undefined;
+        $scope.isEditing = false;
+        $scope.isAdding = false;
+        $scope.isViewMode = true;
+    }
+
+    $scope.enableAddMode = function () {
+        $scope.ticket = undefined;
+        $scope.isEditing = false;
+        $scope.isAdding = true;
+        $scope.isViewMode = false;
+    }
 
     $scope.init = function () {
+
+        $scope.enableViewMode();
 
         $http.get('http://localhost:58952/Home/All').
                   success(function (data, status, headers, config) {
@@ -22,29 +43,55 @@ controllersM.controller('MainCntl', function ($scope, $http) {
                   });
     };
 
+    $scope.details = function (ticket) {
+        $scope.ticket = ticket;
+    }
+
+
 
     $scope.add = function (ticket) {
 
-        $scope.tickets.push(ticket);
-        /*$http.post('http://localhost:58952/Home/Create').
+        $http.post('http://localhost:58952/Home/Create', ticket).
                   success(function (data, status, headers, config) {
-                      $scope.tickets = data;
+                      var copy = JSON.parse(JSON.stringify(ticket));
+                      $scope.tickets.push(copy);
+                      $scope.enableViewMode();
                   }).
                   error(function (data, status, headers, config) {
                       alert("error data load, will be used test data");
-                  });*/
+                      $scope.enableViewMode();
+                  });
     };
 
-    /*
-    $scope.update = function () {
+    $scope.remove = function (ticket) {
+        var strId = ticket.Id.toString();
+        var url = 'http://localhost:58952/Home/Delete/' + strId;
+        console.log(url);
+        $http.delete(url).
+                  success(function (data, status, headers, config) {
+                      var index = $scope.tickets.indexOf(ticket);
+                      $scope.tickets.splice(index, 1);
+                  }).
+                  error(function (data, status, headers, config) {
+                      alert("delete error"+data);
+                  });
+    };
 
-        $http.put('http://localhost:58952/Home/Update', $scope.place).
-            success(function (data, status, headers, config) {
-                //$scope.message = "Запись добавлена, можете добавить еще одну";
-                $scope.place = undefined;
-            });
+    
+    $scope.update = function (ticket) {
 
-    }*/
+        $http.put('http://localhost:58952/Home/Update', ticket).
+                  success(function (data, status, headers, config) {
+                      $scope.enableViewMode();
+                  }).
+                  error(function (data, status, headers, config) {
+                      alert("error data load, will be used test data");
+                      $scope.enableViewMode();
+                  });
+    };
+
+
+    
 
 
 });
